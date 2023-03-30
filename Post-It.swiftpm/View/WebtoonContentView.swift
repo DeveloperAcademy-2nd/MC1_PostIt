35c4e2c25eba4f293812e955d45d90c4bc020c57
Benny(Gibaek Han) <hkb31415@gmail.com>
2023-03-31 00:04:25 +0900

    230331 image, video, view 모두 ForEach문 내에서 관리 가능하도록 코드 수정

--- Post-It.swiftpm/View/WebtoonContentView.swift
+++ Post-It.swiftpm/View/WebtoonContentView.swift
@@ -8,79 +8,160 @@
 import SwiftUI
 import AVKit
 
+struct Content {
+    //    var id: Int
+    var image: Image?
+    var video: AVPlayer?
+    var someView: AnyView?
+}
+
 struct WebtoonContentView: View {
-    var images: [Image]
-    var videos: [AVPlayer]
+    @State var contents: [Content] = []
+    @State var totalCount: Int = 0
+//    @State var idxRange: [Int] = []
+//    let images = [Image("#1"), Image("#2")]
     
     @State var index = 0
     
     var body: some View {
-        let totalCount = images.count + videos.count
+        
         VStack {
+//            Text(idxRange.description)
+//                .font(.largeTitle)
             TabView(selection: $index) {
-                ForEach(0..<4) {i in
-                    images[i]
-                        .resizable()
-                        .scaledToFit()
-                        .tag(i)
+                ForEach(0..<contents.count, id: \.self) { i in
+                    if let image = contents[i].image {
+                        image
+                            .resizable()
+                            .scaledToFit()
+                    } else if let player = contents[i].video {
+                        VideoPlayer(player: player)
+                            .scaledToFit()
+                    } else if let someView = contents[i].someView {
+                        someView
+                    }
                 }
-                VideoPlayer(player: videos[0])
-                    .scaledToFit()
-                    .tag(4)
-                VideoPlayer(player: videos[1])
-                    .scaledToFit()
-                    .tag(5)
-                VideoPlayer(player: videos[2])
-                    .scaledToFit()
-                    .tag(6)
-                images[1]
-                    .resizable()
-                    .scaledToFit()
-                    .tag(7)
+                //                QnA1View()
+                //                Image("#1")
+                //                ForEach(0..<5, id: \.self) {i in
+                //                    if let image = contents[i].image {
+                //                        image
+                //                            .resizable()
+                //                            .scaledToFit()
+                //                        //                            .tag(i)
+                //                    } else if let player = contents[i].video {
+                //                        VideoPlayer(player: player)
+                //                            .scaledToFit()
+                //                            .tag(i)
+                //                    } else if let aView = contents[i].aView {
+                //                        aView
+                //                            .tag(i)
+                //                    }
+                //                    images[i]
+                //                        .resizable()
+                //                        .scaledToFit()
+                //                        .tag(i)
+                //                }
+                //                VideoPlayer(player: videos[0])
+                //                    .scaledToFit()
+                //                    .tag(4)
+                //                VideoPlayer(player: videos[1])
+                //                    .scaledToFit()
+                //                    .tag(5)
+                //                VideoPlayer(player: videos[2])
+                //                    .scaledToFit()
+                //                    .tag(6)
+                //                images[1]
+                //                    .resizable()
+                //                    .scaledToFit()
+                //                    .tag(7)
             }
             .tabViewStyle(.page(indexDisplayMode: .never))
+            //                .tabViewStyle(.page)
             .ignoresSafeArea()
+            .onAppear() {
+//                print("before function contents: \(contents)")
+                
+                for i in 0...3 {
+                    contents.append(Content(image: Image("#\(i)")))
+                }
+                contents.append(Content(video: AVPlayer(url: Bundle.main.url(forResource: "cake", withExtension: "mp4")!)))
+                contents.append(Content(video: AVPlayer(url: Bundle.main.url(forResource: "chair", withExtension: "mp4")!)))
+                contents.append(Content(someView: AnyView(Sample1View())))
+                contents.append(Content(someView: AnyView(Sample2View())))
+                
+                totalCount = contents.count
+                
+//                print("after function contents: \(contents)")
+            }
             .onChange(of: index) { newIndex in
-
-                switch newIndex {
-                case 4:
-                    pauseAllPlayers()
-                    videos[0].seek(to: .zero)
-                    videos[0].play()
-                case 5:
-                    pauseAllPlayers()
-                    videos[1].seek(to: .zero)
-                    videos[1].play()
-                case 6:
-                    pauseAllPlayers()
-                    videos[2].seek(to: .zero)
-                    videos[2].play()
-                default:
-                    pauseAllPlayers()
+                pauseAllPlayers()
+                if let player = contents[newIndex].video {
+                    player.seek(to: .zero)
+                    player.play()
                 }
             }
-            ProgressView((""), value: Double(index+1), total: Double(8))
         }
+        ProgressView((""), value: Double(index+1), total: Double(totalCount))
     }
-    
-    init()  {
-        let player0 = AVPlayer(url: Bundle.main.url(forResource: "cake", withExtension: "mp4")!)
-        let player1 = AVPlayer(url: Bundle.main.url(forResource: "chair", withExtension: "mp4")!)
-        let player2 = AVPlayer(url: Bundle.main.url(forResource: "cake2", withExtension: "mp4")!)
-        
-        self.images = []
-        self.videos = [player0, player1, player2]
-        
-        for i in 0...3 {
-            self.images.append(Image("#\(i)"))
-        }
-        
-    }
-    
+    //        .onAppear {
+    //            self.totalCount = contents.count
+    //
+    //        }
     func pauseAllPlayers() {
-        videos[0].pause()
-        videos[1].pause()
-        videos[2].pause()
+        for content in contents{
+            if let player = content.video {
+                player.pause()
+            }
+        }
+    }
+}
+
+//    init()  {
+//        let player0 = AVPlayer(url: Bundle.main.url(forResource: "cake", withExtension: "mp4")!)
+//        let player1 = AVPlayer(url: Bundle.main.url(forResource: "chair", withExtension: "mp4")!)
+//        let player2 = AVPlayer(url: Bundle.main.url(forResource: "cake2", withExtension: "mp4")!)
+//
+//        //        self.images = []
+//        //        self.videos = [player0, player1, player2]
+//        var contents: [Content] = []
+//        for i in 0...3 {
+//            contents.append(Content(image: Image("#\(i)")))
+//        }
+//        contents.append(Content(video: player0))
+//        contents.append(Content(video: player1))
+//        contents.append(Content(video: player2))
+//
+//        print(contents)
+////        contents.append(Content(aView: AnyView(Sample1View())))
+////        contents.append()
+//
+//        self.contents = contents
+//        //        for i in 0...3 {
+//        //            self.images.append(Image("#\(i)"))
+//        //        }
+//    }
+
+//}
+
+//struct TabItemView: View {
+//    let content: View
+//    var body: some View {
+//        content
+//    }
+//}
+
+struct Sample1View: View {
+    var body: some View {
+        Text("Sample1")
+            .font(.largeTitle)
+    }
+}
+
+struct Sample2View: View {
+    var body: some View {
+        Text("Sample2")
+            .font(.largeTitle)
     }
 }
 
